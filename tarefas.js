6f8e042d46784b449430fae194e8ad1e6224708f
alan7s <alan7@outlook.com>
2021-12-15 13:57:23 -0300

    Exercício 0: Representar tarefas

---
+++ tarefas.js
@@ -0,0 +1,12 @@
+let tarefas = [
+    {
+        nome: 'Comprar leite',
+        categoria: 'compras',
+        realizada: false
+    },
+    {
+        nome: 'Escutar chimbinha',
+        categoria: 'lazer',
+        realizada: true
+    }
+];
