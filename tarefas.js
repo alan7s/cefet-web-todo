eed18fb63d1c7d11320a1e689bb5d8555f5e028c
alan7s <alan7@outlook.com>
2021-12-15 14:17:19 -0300

    Exercício 1: Carregar tarefas existentes

--- tarefas.js
+++ tarefas.js
@@ -10,3 +10,21 @@ let tarefas = [
         realizada: true
     }
 ];
+
+function insereTarefaNaPagina(tarefa) {
+    let listaTarefas = document.querySelector('ul#lista-tarefas')
+    let tarefaEl = document.createElement('li');
+
+    tarefaEl.innerHTML = `${tarefa.nome}`;
+    tarefaEl.classList.add(`item-tarefa`, `categoria-${tarefa.categoria}`);
+
+    if (tarefa.realizada) {
+        tarefaEl.classList.add('marcado');
+    }
+
+    listaTarefas.appendChild(tarefaEl);
+}
+
+for(tarefa of tarefas){
+    insereTarefaNaPagina(tarefa);
+}
